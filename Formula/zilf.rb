86f460be594d55f919c505f3c348217128aa2c3f
Christian Kündig <christian@kuendig.info>
2023-03-16 20:05:00 +0100

    Fix broken formula

--- Formula/zilf.rb
+++ Formula/zilf.rb
@@ -1,10 +1,12 @@
 class Zilf < Formula
   desc "Tools for working with the Zork Implementation Language from Infocom"
-  homepage "https://bitbucket.org/jmcgrew/zilf/"
-  url "https://bitbucket.org/jmcgrew/zilf/downloads/zilf-0.8.zip"
+  
+  # moved in 2021 https://bitbucket.org/jmcgrew/zilf/commits/f90774a9c5e219fd9c5da9ef112f3a40c5af1eb7
+  #homepage "https://bitbucket.org/jmcgrew/zilf/"
+  homepage "https://www.zilf.io/"
+  url "https://www.ifarchive.org/if-archive/infocom/compilers/zilf/zilf-0.8.zip"
   sha256 "60b38260e868910095a6351e5c635ebf58e12dbd66de9677665664ccbd603286"
-
-  bottle :unneeded
+  #bottle :unneeded
 
   depends_on "mono"
 
